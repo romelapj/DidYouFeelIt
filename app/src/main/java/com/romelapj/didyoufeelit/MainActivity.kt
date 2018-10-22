57deda09185cd7b2dedff40e3e4b9d7120ed5baa
Julio Cesar Garcia Sabogal <julio.garcia@rappi.com>
2018-10-22 08:10:51 -0500

    hi

--- app/src/main/java/com/romelapj/didyoufeelit/MainActivity.kt
+++ app/src/main/java/com/romelapj/didyoufeelit/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Start task
+        //Start task as
         EarthquakeAsyncTask().execute(USGS_REQUEST_URL)
     }
 
